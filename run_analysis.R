85893f7bc51115e90cfcae41a4b61f04dfe5dc71
Joe M <33668287+jmoodey@users.noreply.github.com>
2018-03-04 16:45:41 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,45 +1,48 @@
 library(tidyr)
 
-# Load Features and load the second column into column_names 
-features <- read.table("features.txt")
-column_names <- as.vector(features[,2])
+# Load Features 
+features <- read.table("features.txt", header=FALSE)
+activity_labels <- read.table("activity_labels.txt", header=FALSE)
 
 # Load the train data sets
-x_train <- read.table("train/X_train.txt",col.names=column_names)
-y_train <- read.table("train/y_train.txt",col.names=c("Activity"))
-subject_train <- read.table("train/subject_train.txt",col.names=c("Subject"))
-
-# Combine all train data sets into one
-train <- cbind(subject_train, y_train, x_train)
+x_train <- read.table("train/X_train.txt",header=FALSE)
+y_train <- read.table("train/y_train.txt",header=FALSE)
+subject_train <- read.table("train/subject_train.txt",header=FALSE)
 
 # Load the test data sets
-x_test <- read.table("test/X_test.txt",col.names=column_names)
-y_test <- read.table("test/y_test.txt",col.names=c("Activity"))
-subject_test <- read.table("test/subject_test.txt",col.names=c("Subject"))
+x_test <- read.table("test/X_test.txt",header=FALSE)
+y_test <- read.table("test/y_test.txt",header=FALSE)
+subject_test <- read.table("test/subject_test.txt",header=FALSE)
 
-# Combine all test data sets into one
-test <- cbind(subject_test, y_test, x_test)
 
 # Merge train and test data sets
-merged_training_tests <- rbind(train, test)
+merged_features <- rbind(x_train, x_test)
+merged_activity <- rbind(y_train, y_test)
+merged_subject <- rbind(subject_train, subject_test)
+# Name the columns
+names(merged_activity) <- c("Activity")
+names(merged_subject) <- c("Subject")
+names(merged_features) <- features$V2
 
+# Merge Subject, Activity and Features together
+combine_columns <- cbind(merged_subject, merged_activity)
+merged_data <- cbind(combine_columns, merged_features)
 
-#mean_std_data <- merged_training_tests[,c(1:2,column_names)]
-mean_std_columns <- merged_training_tests[,c(1:2,grep("mean()",colnames(merged_training_tests)),grep("std()",colnames(merged_training_tests)))]
+# Extract Mean and Standard Deviation
+stdMeanColumns <- features$V2[grep("mean\\(\\)|std\\(\\)", features$V2)]
 
-# Assign activity descriptions
-activity <- read.table("activity_labels.txt")
-merged_training_tests$Activity <- factor(merged_training_tests$Activity,levels= activity[,1], labels= activity[,2]) 
-merged_training_tests$Subject <- as.factor(merged_training_tests$Subject)
+# Select the required columns 
+required_columns <- c("Subject", "Activity", as.character(stdMeanColumns))
+required_data <- subset(merged_data, select=required_columns)
 
-# Rename columns with better description. 
-Better_Col_Names <- sub("^f","Frequency",sub("^t","Time",colnames(merged_training_tests)))
-colnames(merged_training_tests) <- Better_Col_Names
+# Update Activity Id with the Activity Label    
+for (i in 1:6){
+    required_data$Activity[required_data$Activity == i] <- as.character(activity_labels[i,2])
+}
 
-# Calculate average
-Mean.Data <- aggregate(merged_training_tests[,3:563],list(merged_training_tests$Subject,merged_training_tests$Activity), mean)
-colnames(Mean.Data)[colnames(Mean.Data)=="Group.1"] <- "Subject"
-colnames(Mean.Data)[colnames(Mean.Data)=="Group.2"] <- "Activity"
+# Calculate average and sort the data
+mean.data<-aggregate(. ~Subject + Activity, required_data, mean)
+mean.data<-mean.data[order(mean.data$Subject,mean.data$Activity),]
 
 # Write to tidy_data.txt 
-write.table(Mean.Data, "tidy_data.txt", row.names = FALSE, quote = FALSE)
+write.table(mean.data, "tidy_data.txt", row.names = FALSE, quote = FALSE)
